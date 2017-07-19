49eb5160b57efa32fbded37b83d5f99b54616c1d
GreenLantern101 <arctangent20@yahoo.com>
2017-07-19 10:52:42 -0500

    Init

---
+++ README.md
@@ -0,0 +1,10 @@
+# ce-incremental
+
+A chrome-extension incremental game prototype.
+
+## Dev
+To test:
+Enable `Developer mode` in Chrome Extensions page. Select `Load unpacked extension`, and browse to local dev directory.
+
+To show console:
+`Right-click popup > inspect popup`
---
+++ popup.js
@@ -0,0 +1,65 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function getCurrentTabUrl(callback) {
+  // Query filter to be passed to chrome.tabs.query - see
+  // https://developer.chrome.com/extensions/tabs#method-query
+  const queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, tabs => {
+    // chrome.tabs.query invokes the callback with a list of tabs that match the
+    // query. When the popup is opened, there is certainly a window and at least
+    // one tab, so we can safely assume that |tabs| is a non-empty array.
+    // A window can only have one active tab at a time, so the array consists of
+    // exactly one tab.
+    const tab = tabs[0];
+
+    // A tab is a plain object that provides information about the tab.
+    // See https://developer.chrome.com/extensions/tabs#type-Tab
+    const url = tab.url;
+
+    // tab.url is only available if the "activeTab" permission is declared.
+    // If you want to see the URL of other tabs (e.g. after removing active:true
+    // from |queryInfo|), then the "tabs" permission is required to see their
+    // "url" properties.
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+
+    callback(url);
+  });
+}
+
+function renderStatus(statusText) {
+  document.getElementById('status').textContent = statusText;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  getCurrentTabUrl(url => {
+
+    renderStatus(url); // display in popup
+
+    // synced using chrome sync
+    /* Even if a user disables syncing, storage.sync will still work. 
+    In this case, it will behave identically to storage.local. */
+    const Storage = chrome.storage.sync;
+    // urls object (associative array)
+    Storage.get('urls', (urls) => {
+      urls[url] += 1; // update times visited site
+
+      renderStatus(urls); // show popup // testing only
+      Storage.set({
+        url: old + 1
+      }, () => {
+        // Notify that we saved.
+        console.log('Url updated');
+      });
+    });
+    // use chrome.storage.local for local storage
+
+  }, errorMessage => {
+    renderStatus('Error: ' + errorMessage);
+  });
+});
